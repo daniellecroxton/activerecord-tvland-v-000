a4329d36e386b4f82ddc360de8a1e1283585f17f
daniellecroxton <danikrist@gmail.com>
2017-09-23 17:52:03 -0500

    Done.

---
+++ db/migrate/009_add_foreign_keys_to_characters.rb
@@ -0,0 +1,6 @@
+class AddForeignKeysToCharacters < ActiveRecord::Migration[4.2]
+  def change
+    add_column :characters, :show_id, :integer
+    add_column :characters, :actor_id, :integer
+  end
+end
